ef45b75a2958b3a32deebc4fef0bfc3827db66f7
kennyk65 <kkrueger@gopivotal.com>
2015-11-12 19:13:34 -0500

    EC2 User Data script
    Script to be placed in User-Data section when launching an EC2 instance

--- user-data-script.sh
+++ user-data-script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# This script goes in "User Data" of your instance.
-# It merely downloads the given script, and executes it.
-# This makes it so you can keep your script elsewhere.
+# Copy and paste this script into the "User Data" section of your instance.
+# It merely downloads the given script, and executes it.  You keep your script elsewhere.
 # Note that user-data scripts in AWS are intended for first-time initialization only.
-yum update -y
+
+# Download the file:
+curl -O https://raw.githubusercontent.com/kennyk65/springxd-on-aws/master/install-spring-xd.sh
+
+# Make it executable:
+chmod 777 install-spring-xd.sh
+
+# Execute it:
+./install-spring-xd.sh
