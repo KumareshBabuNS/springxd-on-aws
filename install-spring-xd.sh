a90cc5e6f36a946ff0137de1f2cfcffbb45ca160
kennyk65 <kkrueger@gopivotal.com>
2015-11-12 19:56:54 -0500

    Update install-spring-xd.sh

--- install-spring-xd.sh
+++ install-spring-xd.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
+# This script does MOST of the install for Spring XD, 
+# but there are still some things you need to do yourself.
+
+# go to Home directory where we have permission to do anything:
+cd ~
+
+# Update packages and install unzip.  yum works on some Linuxes, apt-get works on others:
+#sudo yum -y update
+#sudo yum -y install unzip
+#sudo apt-get install unzip 
+
+# Download Spring XD, takes about 3 minutes:
+curl -O http://repo.spring.io/libs-release/org/springframework/xd/spring-xd/1.2.1.RELEASE/spring-xd-1.2.1.RELEASE-dist.zip 
+
+# Unzip:
+unzip spring-xd-1.2.1.RELEASE-dist 
+
+# Move:
+sudo mv spring-xd-1.2.1.RELEASE /opt/ 
+
+# Symbolic link for ease of use:
+sudo ln -s /opt/spring-xd-1.2.1.RELEASE /opt/spring-xd
+
 echo "end of script"
