37cc29129605a49ff44e2b424d8948a132353ca5
kennyk65 <kkrueger@gopivotal.com>
2015-11-13 22:22:18 -0500

    Create userdata-install-launch-zookeeper.sh

---
+++ userdata-install-launch-zookeeper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copy and paste this script into the "User Data" section of your instance.
+# It merely downloads the given script, and executes it.  You keep your script elsewhere.
+# Note that user-data scripts in AWS are intended for first-time initialization only.
+
+# Download the file:
+curl -O https://raw.githubusercontent.com/kennyk65/springxd-on-aws/master/install-launch-zookeeper.sh
+
+# Make it executable:
+chmod 777 install-launch-zookeeper.sh
+
+# Execute it:
+./install-launch-zookeeper.sh
